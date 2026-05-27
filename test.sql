37b9c23a7971db504b31285443fdd9acc5e61047
yefet.bentili@gmail.com <yefet.bentili@gmail.com>
2026-05-27 23:47:05 +0000

    second commit

--- test.sql
+++ test.sql
@@ -1,3 +1,2 @@
-# this is a gold table mock query from databricks
-
-select * from labuser13438968_1779915211.default.chol_age_agg
+-- second commit
+select * from labuser13438968_1779915211.default.health_silver
